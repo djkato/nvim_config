323e1007919956439855b11dbe82aa2f6dc49c21
Djkáťo <djkatovfx@gmail.com>
2024-04-05 01:12:08 +0200

    migrate to v4

---
+++ lua/community.lua
@@ -0,0 +1,29 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.lsp.ts-error-translator-nvim" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.scrolling.satellite-nvim" },
+  { import = "astrocommunity.scrolling.vim-smoothie" },
+  { import = "astrocommunity.media.presence-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  -- import/override with your plugins folder
+}
